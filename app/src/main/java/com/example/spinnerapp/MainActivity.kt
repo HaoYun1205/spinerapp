e70be996dec946292619c9e39007361d2fdb16da
HaoYun1205 <c11007019@gapps.knu.edu.tw>
2022-10-26 20:48:40 +0800

    加入年齡 婚姻建議

--- app/src/main/java/com/example/spinnerapp/MainActivity.kt
+++ app/src/main/java/com/example/spinnerapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.spinnerapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Spinner
 import android.widget.Toast
 
@@ -13,12 +14,28 @@ class MainActivity : AppCompatActivity() {
 
         val spGender = findViewById<Spinner>(R.id.spGender)
         val btOk= findViewById<Button>(R.id.btOk)
+        val etAge=findViewById<EditText>(R.id.etAge)
         btOk.setOnClickListener {
-            Toast
-                .makeText(this,"${spGender.selectedItem.toString()}",Toast.LENGTH_LONG)
-                .show()
-
-
+            val age=etAge.text.toString().toInt()
+            if(spGender.selectedItemPosition==0){
+                when{
+                    age<28->Toast.makeText(this,
+                        getString(R.string.no_hurry),Toast.LENGTH_LONG).show()
+                    age in 28..32 ->Toast.makeText(this,
+                        getString(R.string.find_couple),Toast.LENGTH_LONG).show()
+                    else->Toast.makeText(this,
+                        getString(R.string.get_married),Toast.LENGTH_LONG).show()
+                }
+            }else{
+                when{
+                    age<28->Toast.makeText(this,
+                        getString(R.string.no_hurry),Toast.LENGTH_LONG).show()
+                    age in 25..30 ->Toast.makeText(this,
+                        getString(R.string.find_couple),Toast.LENGTH_LONG).show()
+                    else->Toast.makeText(this,
+                        getString(R.string.get_married),Toast.LENGTH_LONG).show()
+                }
+            }
         }
     }
 }
